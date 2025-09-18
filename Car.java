cd8f472bb8f2e10ffcb83770e2ead3f224f0a0ff
salahaddinayyubiibn-hash <salahaddinayyubiibn@gmail.com>
2025-09-18 21:56:05 +0600

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,39 +1,25 @@
-public final class Car {
+package kz.edu.patterns.builder;
+
+public class Car {
     private final int seats;
     private final Engine engine;
     private final boolean hasGPS;
     private final boolean hasTripComputer;
 
-    private Car(Builder builder) {
-        this.seats = builder.seats;
-        this.engine = builder.engine;
-        this.hasGPS = builder.hasGPS;
-        this.hasTripComputer = builder.hasTripComputer;
-    }
-
-    public int getSeats() { return seats; }
-    public Engine getEngine() { return engine; }
-    public boolean hasGPS() { return hasGPS; }
-    public boolean hasTripComputer() { return hasTripComputer; }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "seats=" + seats +
-                ", engine=" + engine +
-                ", hasGPS=" + hasGPS +
-                ", hasTripComputer=" + hasTripComputer +
-                '}';
+    private Car(int seats, Engine engine, boolean gps, boolean tripComp) {
+        this.seats = seats;
+        this.engine = engine;
+        this.hasGPS = gps;
+        this.hasTripComputer = tripComp;
     }
 
     public static class Builder {
-        private int seats = 4;
-        private Engine engine; // required
-        private boolean hasGPS = false;
-        private boolean hasTripComputer = false;
+        private int seats;
+        private Engine engine;
+        private boolean hasGPS;
+        private boolean hasTripComputer;
 
         public Builder setSeats(int seats) {
-            if (seats <= 0) throw new IllegalArgumentException("seats must be > 0");
             this.seats = seats;
             return this;
         }
@@ -43,34 +29,18 @@ public final class Car {
             return this;
         }
 
-        public Builder enableGPS() {
-            this.hasGPS = true;
+        public Builder setGPS(boolean gps) {
+            this.hasGPS = gps;
             return this;
         }
 
-        public Builder disableGPS() {
-            this.hasGPS = false;
-            return this;
-        }
-
-        public Builder enableTripComputer() {
-            this.hasTripComputer = true;
-            return this;
-        }
-
-        public Builder disableTripComputer() {
-            this.hasTripComputer = false;
+        public Builder setTripComputer(boolean tripComp) {
+            this.hasTripComputer = tripComp;
             return this;
         }
 
         public Car build() {
-            validate();
-            return new Car(this);
-        }
-
-        private void validate() {
-            if (engine == null) throw new IllegalStateException("Engine is required");
-            if (seats < 1) throw new IllegalStateException("seats must be >= 1");
+            return new Car(seats, engine, hasGPS, hasTripComputer);
         }
     }
 }
